27c356fed2b20c6bfc0b7d7d438cbd27f0fea3b0
jedahan <jedahan@talon.(none)>
2009-03-02 17:14:32 -0500

    fix

--- hooks/autopatch.bash
+++ hooks/autopatch.bash
@@ -3,15 +3,19 @@
 
 # This hook autopatches sources. Copy or symlink this script into:
 #
-#   /usr/share/paludis/hooks/auto
+#    /usr/share/paludis/hooks/ebuild_prepare_pre
+#    /usr/share/paludis/hooks/install_all_post
 #
 # You may also want to set $PALUDIS_HOOKS_AUTO_PATCH_DIR in /etc/paludis/bashrc
+
+case "${EBUILD_PHASE}" in
+    compile|config|configure|fetch|init|metadata|nofetch|postinst|postrm|preinst|prerm|setup|strip|test|tidyup|unpack) return
+esac
 
 source ${PALUDIS_ECHO_FUNCTIONS_DIR:-${PALUDIS_EBUILD_DIR}}/echo_functions.bash
 
 AUTOPATCH_DIR="${PALUDIS_HOOKS_AUTO_PATCH_DIR}/${CATEGORY}/${PN}"
 AUTOPATCHED_FILE="${PALUDIS_HOOKS_TMPDIR:-${ROOT}/var/tmp/paludis}/._autopatched_${PALUDIS_PID}"
-
 
 case "${HOOK}" in
     ebuild_prepare_pre)
