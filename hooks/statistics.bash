778457497e68de9a47b64404faa1ef4e53035979
Jonathan Dahan <jedahan@gmail.com>
2009-06-17 01:00:53 +0000

    determine repository locations

--- hooks/statistics.bash
+++ hooks/statistics.bash
@@ -11,9 +11,13 @@ pInstalled=`paludis --list-packages --repository installed | grep -c "*"`
 pAvailable=`cave print-packages | grep -v user/ | grep -v group/ | grep -v virtual/ | wc -l`
 
 rInstalled=`cave print-repositories | grep -v account | grep -v installed | grep -v unwritten | grep -v unavailable | wc -l`
-rOfficial=`ls /var/db/paludis/repositories/unavailable/*repository | wc -l`
+
+rLocation=`paludis --configuration-variable unavailable location`
+ruLocation=`paludis --configuration-variable unavailable-unofficial location`
+
+rOfficial=`ls $rLocation/*repository | wc -l`
 let "rOfficial += 1" # arbor
-rUnofficial=`ls /var/db/paludis/repositories/unavailable-unofficial/*repository | wc -l`
+rUnofficial=`ls $ruLocation/*repository | wc -l`
 let "rAvailable = $rOfficial + $rUnofficial"
 
 echo "    packages: $pInstalled installed    $pAvailable available"
