64bad85e9665c2759eed36997c5c4da0d5a08d00
Jonathan Dahan <jedahan@gmail.com>
2009-06-19 02:38:28 +0000

    consolidation and regular expressions

--- hooks/statistics.bash
+++ hooks/statistics.bash
@@ -4,21 +4,19 @@
 # Show installed and available packages, repositories. Symlink to:
 #
 #  /usr/share/paludis/hooks/sync_all_post 
-#  /usr/share/paludis/hooks/sync_post
 #
 
 pInstalled=`paludis --list-packages --repository installed | grep -c "*"`
-pAvailable=`cave print-packages | grep -v user/ | grep -v group/ | grep -v virtual/ | wc -l`
+pAvailable=`cave print-packages | egrep -vc 'user/|group/|virtual/'`
 
-rInstalled=`cave print-repositories | grep -v account | grep -v installed | grep -v unwritten | grep -v unavailable | wc -l`
+rInstalled=`cave print-repositories | egrep -vc 'account|installed|un'`
 
-rLocation=`paludis --configuration-variable unavailable location`
-ruLocation=`paludis --configuration-variable unavailable-unofficial location`
+    rLoc=`paludis --configuration-variable unavailable location`
+    ruLoc=`paludis --configuration-variable unavailable-unofficial location`
 
-rOfficial=`ls $rLocation/*repository | wc -l`
-let "rOfficial += 1" # arbor
-rUnofficial=`ls $ruLocation/*repository | wc -l`
-let "rAvailable = $rOfficial + $rUnofficial"
+rAvailable=`ls {$rLoc,$ruLoc}/* | wc -l`
+let "rAvailable += 1" # arbor
+
 
 echo "    packages: $pInstalled installed    $pAvailable available"
 echo "repositories:  $rInstalled installed      $rAvailable available"
