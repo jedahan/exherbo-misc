8aac6730b60384a67a9af2be8b7d08d23fee2c0d
Jonathan Dahan <jedahan@gmail.com>
2009-08-23 12:33:20 -0400

    remove file extensions

--- scripts/remove-redundant-deps.sh
+++
@@ -1,9 +0,0 @@
-for p in `paludis -k system | grep \/`; do 
-    for o in `grep $p /var/paludis/repositories/*/packages/*/*/*ex{heres-0,libs}`; do
-        #remove here?
-        
-        if `$o | cut -d':' -f1` -eq 
-        $o
-    
-    ; 
-done
